302996380ea48e82b4604f26596398dd21765ef6
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-12-01 08:33:14 +0000

    Implement calculate_increased_depths using window.fold

--- 2021/day1/src/depth.rs
+++ 2021/day1/src/depth.rs
@@ -1,5 +1,7 @@
 pub fn calculate_increased_depths(readings: Vec<u32>) -> u32 {
-    0
+    readings
+        .windows(2)
+        .fold(0, |acc, x| if x[0] < x[1] { acc + 1 } else { acc })
 }
 
 #[cfg(test)]
@@ -14,5 +16,14 @@ mod tests {
         let actual_increases = calculate_increased_depths(readings);
 
         assert_eq!(actual_increases, 0);
+    }
+
+    #[test]
+    fn calculate_increases() {
+        let readings: Vec<u32> = vec![100, 101, 102, 101, 100];
+
+        let actual_increases = calculate_increased_depths(readings);
+
+        assert_eq!(actual_increases, 2);
     }
 }
--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,9 +1,21 @@
-// use crate::depth;
+use std::fs;
 
 pub mod depth;
 
 fn main() {
-    let increases = depth::calculate_increased_depths(vec![]);
+    let contents: String =
+        fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let readings: Vec<u32> = contents
+        .split("\n")
+        .filter(|line| line.is_empty() == false)
+        .map(|line| {
+            line.parse::<u32>()
+                .expect("Error parsing input, expected a number.")
+        })
+        .collect();
+
+    let increases = depth::calculate_increased_depths(readings);
 
     println!("Increases = {}", increases)
 }
